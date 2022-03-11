4fca7d4403358f72534d6b52fd5489462d8ab7ee
Katsuhiro Kakoi <katsuhiro.k1215@gmail.com>
2022-03-11 23:46:16 +0900

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(document).ready(function() {
+  $('.slider').slick({
+    accessibility: false,
+    fade: true,
+    autoplay: true,
+    infinite: true,
+    dots: false,
+    arrows: false,
+    autoplaySpeed: 3000,
+    speed: 3000,
+});
+});
